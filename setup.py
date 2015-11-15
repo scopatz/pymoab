477c5403dbc88549480edf9fb0d085a0341f7fb2
pshriwise <shriwise@wisc.edu>
2015-11-15 07:37:32 -0600

    Updating build to search environment for moab path.

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
-
+import os
 import numpy as np
 from Cython.Build import cythonize
 
-include_path = [np.get_include(),"/home/shriwise/dagmc_blds/moabs/include/"]
+moab_env_var = 'MOAB_PATH'
+
+try:
+    moab_root = os.environ[moab_env_var]
+except KeyError:
+    raise EnvironmentError('MOAB_PATH not found in environment.')
+
+moab_include = moab_root + '/include/'
+moab_lib = moab_root + '/lib/'
+
+include_path = [np.get_include(),moab_include]
 
 ext_modules = cythonize('pymoab/*.pyx', language='c++', 
                         include_dirs=include_path)
 for ext in ext_modules:
     ext.include_dirs = include_path
-    ext.library_dirs = ["/home/shriwise/dagmc_blds/moabs/lib/"]
+    ext.library_dirs = [moab_lib]
     ext.libraries = ["MOAB"]
 
 setup(
