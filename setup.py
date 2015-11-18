b663c1deae1c838af74d32ea628b9862ba159239
pshriwise <shriwise@wisc.edu>
2015-11-18 09:02:56 -0600

    Added comments. Wrapped default location check into the environment check.

--- setup.py
+++ setup.py
@@ -7,19 +7,27 @@ from Cython.Build import cythonize
 
 moab_env_var = 'MOAB_PATH'
 moab_root = None
+moab_default = '/usr/'
 
+#look for command line argument
 for arg in sys.argv:
     if "--moab-path" in arg:
         moab_root = arg.split("=")[-1]
         print "Set MOAB location with user-provided install path."
         sys.argv.remove(arg)
 
+#search environment for moab install
 if moab_root is None:
     try:
         moab_root = os.environ[moab_env_var]
     except KeyError:
-        raise EnvironmentError('MOAB_PATH not found in environment.')
+        #as a last attempt, check the root location
+        if os.path.isfile(moab_default+'/include/moab/Core.hpp'):
+            moab_root = moab_default
+        else:
+            raise EnvironmentError('MOAB_PATH not found in environment.')
 
+#check that the moab location is legitimate
 if not os.path.isfile(moab_root+'/include/moab/Core.hpp'):
     raise StandardError('Provided MOAB location is invalid.')
 
