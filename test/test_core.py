4fb40c57268228f2c97a551833841c87e9debca8
pshriwise <shriwise@wisc.edu>
2015-11-24 14:44:53 -0600

    Expanded tag test to include the double type.

--- test/test_core.py
+++ test/test_core.py
@@ -19,13 +19,13 @@ def test_tags():
 
     mb = core.Core()
 
-    test_tag = mb.tag_get_handle("Test",1,types.MB_TYPE_INTEGER)
-
     coord = np.array((1,1,1),dtype='float64')
 
     vert = mb.create_vertices(coord)
 
     vert_copy = np.array((vert[0],),dtype='uint64')
+
+    test_tag = mb.tag_get_handle("Test",1,types.MB_TYPE_INTEGER)
 
     test_val = 4
     
@@ -38,6 +38,21 @@ def test_tags():
     assert len(data) == 1
     
     assert data[0] == test_val 
+
+    test_tag = mb.tag_get_handle("Test1",1,types.MB_TYPE_DOUBLE)
+
+    test_val = 4.4
+
+    test_tag_data = np.array((test_val),dtype='float64')
+
+    mb.tag_set_data(test_tag, vert_copy, test_tag_data)
+
+    data = mb.tag_get_data(test_tag, vert_copy)
+
+    assert len(data) == 1
+
+    assert data[0] == test_val
+
     
 
     
