6894146d6a60ef7eec137486a843761d041de67a
pshriwise <shriwise@wisc.edu>
2015-11-23 23:26:19 -0600

    Adding first unit test.

---
+++ test/test_core.py
@@ -0,0 +1,13 @@
+from pymoab import core
+import numpy as np
+import os
+
+def test_write_mesh():
+
+    mb = core.Core()
+
+    mb.create_vertices(np.ones(3))
+
+    mb.write_file("outfile.h5m")
+
+    assert os.path.isfile("outfile.h5m")
