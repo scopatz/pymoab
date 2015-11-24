a40476470e83903ca132c54986c37fdcbfee33f4
pshriwise <shriwise@wisc.edu>
2015-11-24 14:31:32 -0600

    Added final asserts to tag test.

--- test/test_core.py
+++ test/test_core.py
@@ -19,8 +19,25 @@ def test_tags():
 
     mb = core.Core()
 
-    test_tag = mb.tag_get_handle("Test",1,2)
+    test_tag = mb.tag_get_handle("Test",1,types.MB_TYPE_INTEGER)
 
+    coord = np.array((1,1,1),dtype='float64')
+
+    vert = mb.create_vertices(coord)
+
+    vert_copy = np.array((vert[0],),dtype='uint64')
+
+    test_val = 4
+    
+    test_tag_data = np.array((test_val),dtype='int')
+    
+    mb.tag_set_data(test_tag, vert_copy, test_tag_data)
+
+    data = mb.tag_get_data(test_tag, vert_copy)
+
+    assert len(data) == 1
+    
+    assert data[0] == test_val 
     
 
     
