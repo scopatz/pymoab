0df0d393323d9b6366f4cb5c09f66c3ca9572013
pshriwise <shriwise@wisc.edu>
2015-12-07 15:16:29 -0600

    Refactored tests a bit. Removed type specifications in arrays.

--- test/test_core.py
+++ test/test_core.py
@@ -14,44 +14,51 @@ def test_write_mesh():
 
     assert os.path.isfile("outfile.h5m")
 
-
-def test_tags():
-
+def testinteger_tag():
+    
     mb = core.Core()
 
-    coord = np.array((1,1,1),dtype='float64')
-
-    vert = mb.create_vertices(coord)
-
-    vert_copy = np.array((vert[0],),dtype='uint64')
+    vh = vertex_handle(mb)
 
     test_tag = mb.tag_get_handle("Test",1,types.MB_TYPE_INTEGER)
 
     test_val = 4
     
-    test_tag_data = np.array((test_val),dtype='int')
+    test_tag_data = np.array((test_val,))
     
-    mb.tag_set_data(test_tag, vert_copy, test_tag_data)
+    mb.tag_set_data(test_tag, vh, test_tag_data)
 
-    data = mb.tag_get_data(test_tag, vert_copy)
+    data = mb.tag_get_data(test_tag, vh)
 
     assert len(data) == 1
     
     assert data[0] == test_val 
 
+def test_double_tag():
+    
+    mb = core.Core()
+
+    vh = vertex_handle(mb)
+
     test_tag = mb.tag_get_handle("Test1",1,types.MB_TYPE_DOUBLE)
 
     test_val = 4.4
 
-    test_tag_data = np.array((test_val),dtype='float64')
+    test_tag_data = np.array((test_val))
 
-    mb.tag_set_data(test_tag, vert_copy, test_tag_data)
+    mb.tag_set_data(test_tag, vh, test_tag_data)
 
-    data = mb.tag_get_data(test_tag, vert_copy)
+    data = mb.tag_get_data(test_tag, vh)
 
     assert len(data) == 1
 
     assert data[0] == test_val
+
+def test_opaque_tag():
+
+    mb = core.Core()
+
+    vh = vertex_handle(mb)
 
     tag_length = 6
 
@@ -61,9 +68,9 @@ def test_tags():
 
     test_tag_data = np.array((test_val,))
 
-    mb.tag_set_data(test_tag, vert_copy, test_tag_data)
+    mb.tag_set_data(test_tag, vh, test_tag_data)
 
-    data = mb.tag_get_data(test_tag, vert_copy)
+    data = mb.tag_get_data(test_tag, vh)
 
     assert len(data) == tag_length
 
@@ -72,3 +79,12 @@ def test_tags():
     assert data.dtype == '|S' + str(tag_length)
 
     
+def vertex_handle(core):
+
+    coord = np.array((1,1,1),dtype='float64')
+
+    vert = core.create_vertices(coord)
+
+    vert_copy = np.array((vert[0],),dtype='uint64')
+
+    return vert_copy
